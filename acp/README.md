906087b878ecf2e1d1c03ee4bd26e32b8de5dad9
Andreas Gruß <agruss90@googlemail.com>
2018-10-25 22:39:41 +0200

    added kinda mockup index for concept and README

---
+++ acp/README.md
@@ -0,0 +1,9 @@
+﻿# Admin Control Panel
+
+The idea is to create a website for the configuration of the apps and the snips-app-collection itself.
+
+Modify your url, port, intents and the snips host address or create a new configuration for a new app.
+
+## Future ideas
+
+Creating a start and stop button for the main components and the apps. This changes would make the shell and powershell script obsolete.
---
+++ acp/routes/index.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+const appConfig = require('../../intent-listener/config.json');
+
+/* GET home page. */
+router.get('/', function (req, res) {
+	var apps = [];
+	appConfig.apps.forEach(app => {
+		const portIndex = 17;
+		var urlEndIndex = app.intents[0].url.indexOf('/', portIndex);
+		
+		var config = {
+			name: app.name,
+			port: app.intents[0].url.substring(portIndex, urlEndIndex)
+		};
+		
+		apps.push(config);
+	});
+
+	res.render('index', {
+		title: 'Admin Control Panel', appNames: apps});
+});
+
+module.exports = router;
